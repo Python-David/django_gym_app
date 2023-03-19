7e63b69bde325975dc5f01864092c45bfb28cdf1
Python-David <mrnwobodo@gmail.com>
2023-03-19 19:41:21 +0100

    created templates and static directory to hold web pages. created custom user model that overrides the django's User model

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="web-home"),
-    path('login/', views.login, name="web-login")
+    path('', views.home, name="web_home"),
+    path('member_login/', views.member_login, name="web_member_login"),
+    path('trainer_login/', views.trainer_login, name="web_trainer_login"),
+    path('register/', views.register, name="web_register"),
+    path('contact/', views.contact, name="web_contact"),
 ]
--- web/views.py
+++ web/views.py
@@ -6,5 +6,17 @@ def home(request):
     return render(request, 'web/home.html')
 
 
-def login(request):
-    return render(request, 'web/login.html')
+def member_login(request):
+    return render(request, 'web/member_login.html')
+
+
+def trainer_login(request):
+    return render(request, 'web/trainer_login.html')
+
+
+def register(request):
+    return render(request, 'web/register.html')
+
+
+def contact(request):
+    return render(request, 'web/contact.html')
